30191716fded117e52fda6802145f94dfcd2dd2c
Nadeem Khan <imkhan.1910@gmail.com>
2023-06-17 00:54:26 +0530

    I include the Series Name

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Details from './Details';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <h1 className='heading__style'>My Favorite Shows List</h1>
+    {  /* earlier we write -> {Details.map(function SCard(val){...} */
+      Details.map((val) => {     // now this is fat arrow representation
+        return(
+          <App 
+            key = {val.id} 
+            img = {val.img}
+            title = {val.title}
+            sName = {val.sName}
+            link = {val.link}
+          />
+        );
+    })}
+
+    {/* <App 
+      img = {Details[1].img}
+      title = {Details[1].title}
+      series = {Details[1].sName}
+      link = {Details[1].link}
+    />
+
+    <App 
+      img = {Details[2].img}
+      title = {Details[2].title}
+      series = {Details[2].sName}
+      link = {Details[2].link}
+    />
+
+    <App 
+      img = {Details[3].img}
+      title = {Details[3].title}
+      series = {Details[3].sName}
+      link = {Details[3].link}
+    />
+
+    <App 
+      img = {Details[4].img}
+      title = {Details[4].title}
+      series = {Details[4].sName}
+      link = {Details[4].link}
+    /> */}
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
